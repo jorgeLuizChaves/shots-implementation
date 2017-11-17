ea7c4bdabb7dbfd9b5a2dd5d20de3a50e76552dc
Jorge Luiz <jorge.luizchaves@yahoo.com.br>
2017-11-17 07:39:59 -0200

    Initial Commit

---
+++ Shots-Implementing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Shots-Implementing
+//
+//  Created by Jorge Luiz on 17/11/17.
+//  Copyright © 2017 Jorge Luiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
